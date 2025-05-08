b5dcda4f138dadb678cd82e1d837515e6953f39f
Mister-Blue-Sky <Mister-Blue-Sky@users.noreply.github.com>
2025-05-08 10:09:59 +0000

    deploy: d41aafc846f1026f5421332b4491630d1a486157

--- search/functions_4.js
+++ search/functions_4.js
@@ -8,11 +8,13 @@ var searchData=
   ['emptyframequeue_5',['emptyFrameQueue',['../namespaceuvgvpcc__enc_1_1API.html#a75dacaf2d79b4a549ecb2a91ae8a6105',1,'uvgvpcc_enc::API']]],
   ['encodeframe_6',['encodeFrame',['../namespaceuvgvpcc__enc_1_1API.html#adbce6afc710547457b42680ba4e6366c',1,'uvgvpcc_enc::API']]],
   ['encodegofmaps_7',['encodegofmaps',['../classAbstract2DMapEncoder.html#aa2abaf03e685666b2337fbd259679392',1,'Abstract2DMapEncoder::encodeGOFMaps()'],['../classEncoderKvazaar.html#a30951e8aa60360399ae186d1df9f208c',1,'EncoderKvazaar::encodeGOFMaps()'],['../namespaceMapEncoding.html#ae076249a61063d310ef6b2209fd17e8a',1,'MapEncoding::encodeGOFMaps()']]],
-  ['evalmetric_8',['evalmetric',['../structnanoflann_1_1L2__Simple__Adaptor.html#a30c0ec8d17fcb1b462938ba4e969bfe9',1,'nanoflann::L2_Simple_Adaptor::evalMetric()'],['../structnanoflann_1_1SO3__Adaptor.html#aa45f6f6d7d3644a2a9fc9b40ad98beb4',1,'nanoflann::SO3_Adaptor::evalMetric()'],['../structnanoflann_1_1SO2__Adaptor.html#ab2ef292806c8d78ff3d08de8ebe0b12c',1,'nanoflann::SO2_Adaptor::evalMetric()'],['../structnanoflann_1_1L2__Adaptor.html#a6a3a2e26a05436d50a9aec206a6c35a9',1,'nanoflann::L2_Adaptor::evalMetric()'],['../structnanoflann_1_1L1__Adaptor.html#a31c28490266c74be58a2bba0af264e36',1,'nanoflann::L1_Adaptor::evalMetric()']]],
-  ['execute_9',['execute',['../classuvgvpcc__enc_1_1Job.html#a958d9038c7a459bd99c7542719ea0b60',1,'uvgvpcc_enc::Job']]],
-  ['exportpointcloud_10',['exportPointCloud',['../utilsPatchGeneration_8hpp.html#a7e03bd9f637d6875e048306bf9f014a2',1,'utilsPatchGeneration.hpp']]],
-  ['extract_5flist_5fproperty_11',['extract_list_property',['../classminiply_1_1PLYReader.html#a2c71e492c56ebee22a9011c76cad0ae9',1,'miniply::PLYReader']]],
-  ['extract_5fproperties_12',['extract_properties',['../classminiply_1_1PLYReader.html#a567b0c1b1aa8ee95da31305f8a0703dc',1,'miniply::PLYReader']]],
-  ['extract_5fproperties_5fwith_5fstride_13',['extract_properties_with_stride',['../classminiply_1_1PLYReader.html#acb9146f0176d6d12701f1e71126a4ee2',1,'miniply::PLYReader']]],
-  ['extract_5ftriangles_14',['extract_triangles',['../classminiply_1_1PLYReader.html#aa51b5c6344acb425b1b5d36c98a8f698',1,'miniply::PLYReader']]]
+  ['evalmetric_8',['evalmetric',['../structnanoflann_1_1SO2__Adaptor.html#ab2ef292806c8d78ff3d08de8ebe0b12c',1,'nanoflann::SO2_Adaptor::evalMetric()'],['../structnanoflann_1_1SO3__Adaptor.html#aa45f6f6d7d3644a2a9fc9b40ad98beb4',1,'nanoflann::SO3_Adaptor::evalMetric()'],['../structnanoflann_1_1L2__Simple__Adaptor.html#a30c0ec8d17fcb1b462938ba4e969bfe9',1,'nanoflann::L2_Simple_Adaptor::evalMetric()'],['../structnanoflann_1_1L2__Adaptor.html#a6a3a2e26a05436d50a9aec206a6c35a9',1,'nanoflann::L2_Adaptor::evalMetric()'],['../structnanoflann_1_1L1__Adaptor.html#a31c28490266c74be58a2bba0af264e36',1,'nanoflann::L1_Adaptor::evalMetric()']]],
+  ['execute_9',['execute',['../classuvgvpcc__enc_1_1Job.html#ad8262f9d72626cd20c465de9f73a69df',1,'uvgvpcc_enc::Job']]],
+  ['executenotimer_10',['executeNoTimer',['../classuvgvpcc__enc_1_1Job.html#a03a6d90c801275c5bb513432b0d11c8e',1,'uvgvpcc_enc::Job']]],
+  ['executetimer_11',['executeTimer',['../classuvgvpcc__enc_1_1Job.html#a03ce044f441a0ceceb61cb04a6ace0d7',1,'uvgvpcc_enc::Job']]],
+  ['exportpointcloud_12',['exportPointCloud',['../utilsPatchGeneration_8hpp.html#a7e03bd9f637d6875e048306bf9f014a2',1,'utilsPatchGeneration.hpp']]],
+  ['extract_5flist_5fproperty_13',['extract_list_property',['../classminiply_1_1PLYReader.html#a2c71e492c56ebee22a9011c76cad0ae9',1,'miniply::PLYReader']]],
+  ['extract_5fproperties_14',['extract_properties',['../classminiply_1_1PLYReader.html#a567b0c1b1aa8ee95da31305f8a0703dc',1,'miniply::PLYReader']]],
+  ['extract_5fproperties_5fwith_5fstride_15',['extract_properties_with_stride',['../classminiply_1_1PLYReader.html#acb9146f0176d6d12701f1e71126a4ee2',1,'miniply::PLYReader']]],
+  ['extract_5ftriangles_16',['extract_triangles',['../classminiply_1_1PLYReader.html#aa51b5c6344acb425b1b5d36c98a8f698',1,'miniply::PLYReader']]]
 ];
--- search/variables_12.js
+++ search/variables_12.js
@@ -3,7 +3,8 @@ var searchData=
   ['tangentaxis_5f_0',['tangentAxis_',['../structuvgvpcc__enc_1_1Patch.html#aa3b535e4200aa6f0a2431a7ebcd7e4f7',1,'uvgvpcc_enc::Patch']]],
   ['theta_1',['theta',['../structPointCloud__Orient_1_1Point.html#a94ab0276b2e3cb6c9bd11ce2708f06d0',1,'PointCloud_Orient::Point']]],
   ['threads_2',['threads',['../structcli_1_1opts__t.html#af37434bb408a128a91363d3dc2ecba69',1,'cli::opts_t']]],
-  ['treecount_5f_3',['treeCount_',['../classnanoflann_1_1KDTreeSingleIndexDynamicAdaptor.html#a2f805b760e60bd0ff24fd1fafe05e7fe',1,'nanoflann::KDTreeSingleIndexDynamicAdaptor']]],
-  ['treeindex_5f_4',['treeindex_',['../classnanoflann_1_1KDTreeSingleIndexDynamicAdaptor__.html#a251ac6fab7ee9b2e731d6ad15d466ec8',1,'nanoflann::KDTreeSingleIndexDynamicAdaptor_::treeIndex_'],['../classnanoflann_1_1KDTreeSingleIndexDynamicAdaptor.html#ab74708de8276edf76d46c7cdf4bed18f',1,'nanoflann::KDTreeSingleIndexDynamicAdaptor::treeIndex_']]],
-  ['type_5',['type',['../structminiply_1_1PLYTypeAlias.html#aeeb846e72e60d2b759216cc54a7040d8',1,'miniply::PLYTypeAlias::type'],['../structminiply_1_1PLYProperty.html#a1744b48649c4def129988bf89e475bd4',1,'miniply::PLYProperty::type'],['../structuvgvpcc__enc_1_1ParameterInfo.html#a86fc4173cd84d308069b1e5a5fe851bb',1,'uvgvpcc_enc::ParameterInfo::type']]]
+  ['timerlog_3',['timerLog',['../structuvgvpcc__enc_1_1Parameters.html#a7daa08aa4174583dc3921a56a1a83b97',1,'uvgvpcc_enc::Parameters']]],
+  ['treecount_5f_4',['treeCount_',['../classnanoflann_1_1KDTreeSingleIndexDynamicAdaptor.html#a2f805b760e60bd0ff24fd1fafe05e7fe',1,'nanoflann::KDTreeSingleIndexDynamicAdaptor']]],
+  ['treeindex_5f_5',['treeindex_',['../classnanoflann_1_1KDTreeSingleIndexDynamicAdaptor__.html#a251ac6fab7ee9b2e731d6ad15d466ec8',1,'nanoflann::KDTreeSingleIndexDynamicAdaptor_::treeIndex_'],['../classnanoflann_1_1KDTreeSingleIndexDynamicAdaptor.html#ab74708de8276edf76d46c7cdf4bed18f',1,'nanoflann::KDTreeSingleIndexDynamicAdaptor::treeIndex_']]],
+  ['type_6',['type',['../structminiply_1_1PLYTypeAlias.html#aeeb846e72e60d2b759216cc54a7040d8',1,'miniply::PLYTypeAlias::type'],['../structminiply_1_1PLYProperty.html#a1744b48649c4def129988bf89e475bd4',1,'miniply::PLYProperty::type'],['../structuvgvpcc__enc_1_1ParameterInfo.html#a86fc4173cd84d308069b1e5a5fe851bb',1,'uvgvpcc_enc::ParameterInfo::type']]]
 ];
