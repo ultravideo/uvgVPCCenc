3221ddf11205a50b8708e859cd26efa5fb994425
Jaakko Laitinen <jaakko.laitinen@tuni.fi>
2025-09-05 17:34:52 +0300

    Update V3CRTP building

--- src/app/dependencies/FindDependencies.cmake
+++ src/app/dependencies/FindDependencies.cmake
@@ -1,50 +1,51 @@
-# Check if uvgV3CRTP is already available
-find_package(uvgV3CRTP 0.3.1 QUIET) #TODO: update min version to a version with necessary bug fixes etc.
+if(ENABLE_V3CRTP)
+	# Check if uvgV3CRTP is already available
+	find_package(uvgV3CRTP 0.6.0 QUIET) #TODO: update min version to a version with necessary bug fixes etc.
 
-# Try pkgConfig as well
-find_package(PkgConfig QUIET)
-if(PkgConfig_FOUND)
-    if(NOT UVGV3CRTP_FOUND)
-	    pkg_search_module(UVGV3CRTP uvgv3crtp>=0.3.1 uvgV3CRTP>=0.3.1)
-	endif()
-endif()
-
-# If we did not find uvgV3CRTP build it from source_group
-if(UVGV3CRTP_FOUND)
-    message(STATUS "Found uvgV3CRTP")
-else()
-    message(STATUS "uvgV3CRTP not found, building from source...")
-	
-	#include(FetchContent)
-	find_package(Git REQUIRED)
-	
-	option(GIT_SUBMODULE "Check submodules during build" OFF) #TODO: set to on when public git available
-	if(GIT_SUBMODULE)
-	    message(STATUS "Update submodule")
-		execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-		                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-						RESULT_VARIABLE GIT_SUBMOD_RESULT)
-	    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-		    message(FATAL_ERROR "git submodule update --init --recursive failed with ${GIT_SUBMOD_RESULT}, please checkout submodule")
+	# Try pkgConfig as well
+	find_package(PkgConfig QUIET)
+	if(PkgConfig_FOUND)
+		if(NOT UVGV3CRTP_FOUND)
+			pkg_search_module(UVGV3CRTP uvgv3crtp>=0.6.0 uvgV3CRTP>=0.6.0)
 		endif()
 	endif()
-	
-	# Add subdir and specify relevant options
-	set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 
-    set(UVGV3CRTP_DISABLE_TESTS ON CACHE BOOL "" FORCE)
-    set(UVGV3CRTP_DISABLE_EXAMPLES  ON CACHE BOOL "" FORCE)
-    set(UVGV3CRTP_DISABLE_INSTALL OFF CACHE BOOL "" FORCE)
+	# If we did not find uvgV3CRTP build it from source_group
+	if(UVGV3CRTP_FOUND)
+		message(STATUS "Found uvgV3CRTP")
+	else()
+		message(STATUS "uvgV3CRTP not found, building from source...")
+		
+		#include(FetchContent)
+		find_package(Git REQUIRED)
+		
+		option(GIT_SUBMODULE "Check submodules during build" ON)
+		if(GIT_SUBMODULE)
+			message(STATUS "Update submodule")
+			execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+							WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+							RESULT_VARIABLE GIT_SUBMOD_RESULT)
+			if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+				message(FATAL_ERROR "git submodule update --init --recursive failed with ${GIT_SUBMOD_RESULT}, please checkout submodule")
+			endif()
+		endif()
+		
+		# Add subdir and specify relevant options
+		set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 
-    add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/dependencies/uvgV3CRTP)
-	
-	include_directories(${uvgv3crtp_SOURCE_DIR}/include)
-	link_directories(${uvgv3crtp_SOURCE_DIR})
-	
-    unset(BUILD_SHARED_LIBS)
+		set(UVGV3CRTP_DISABLE_TESTS ON CACHE BOOL "" FORCE)
+		set(UVGV3CRTP_DISABLE_EXAMPLES  ON CACHE BOOL "" FORCE)
+		set(UVGV3CRTP_DISABLE_INSTALL OFF CACHE BOOL "" FORCE)
 
+		add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/dependencies/uvgV3CRTP)
+		
+		include_directories(${uvgv3crtp_SOURCE_DIR}/include)
+		link_directories(${uvgv3crtp_SOURCE_DIR})
+		
+		unset(BUILD_SHARED_LIBS)
+
+	endif()
 endif()
-
 
 # include and link directories for dependencies. 
 # These are needed when the compilation happens a second time and the library 
